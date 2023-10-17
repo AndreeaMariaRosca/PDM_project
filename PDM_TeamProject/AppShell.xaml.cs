626fcecfd1ff69416aa855e9ff5291f082636b2e
Andreea Rosca <andreea.rosca204@gmail.com>
2023-10-17 18:57:21 +0300

    setup project

---
+++ PDM_TeamProject/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PDM_TeamProject
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PDM_TeamProject/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PDM_TeamProject
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
