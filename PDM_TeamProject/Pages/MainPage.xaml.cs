febfa8bdaf4070ae3be494888f4c32662c42c75c
Andreea Rosca <andreea.rosca204@gmail.com>
2023-11-05 21:38:26 +0200

    added main page logic

--- PDM_TeamProject/Pages/MainPage.xaml.cs
+++ PDM_TeamProject/Pages/MainPage.xaml.cs
@@ -1,8 +1,36 @@
+using PDM_TeamProject.Classes;
 namespace PDM_TeamProject.Pages;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-	}
+    SerieService MovieService;
+    SerieDatabase MovieDatabase;
+
+    public MainPage(SerieService movieService, SerieDatabase movieDatabase)
+    {
+        // InitializeComponent();
+        MovieService = movieService;
+        MovieDatabase = movieDatabase;
+    }
+
+    protected override async void OnAppearing()
+    {
+        //List<Genre> genres = await SerieService.GetGenres();
+        //SerieDatabase.Genres.InsertOrUpdateGenres(genres);
+    }
+
+    private void OnBrowseMoviesClick(object sender, EventArgs e)
+    {
+        ((Shell)Application.Current.MainPage).GoToAsync("BrowseSeriesPage");
+    }
+
+    private void OnMyListClicked(object sender, EventArgs e)
+    {
+        ((Shell)Application.Current.MainPage).GoToAsync("MyListPage");
+    }
+
+    private void OnStatisticsClicked(object sender, EventArgs e)
+    {
+        ((Shell)Application.Current.MainPage).GoToAsync("StatisticsPage");
+    }
 }
